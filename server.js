6eb1da68355efeb662f368363ef5fdbcba9dd0a3
Shilpa Tripathi <shilpat@uci.edu>
2018-05-14 23:00:26 -0700

    changes code

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /* Include the static file webserver library*/
-var static = require('node-static);
+var static = require('node-static');
 /* Include the http server library */
 var http = require('http');
 /* Assume that we are running on Heroku */
@@ -17,8 +17,8 @@ var file = new static.Server (directory);
 /* Construct an http server that gets files from the file server */
 var app = http.createServer(
 		function(request,response) {
-				request.addListener ( 'end',
-						function () {
+				request.addListener ('end',
+						function() {
 				file.serve (request,response);
 			}
 		).resume();
