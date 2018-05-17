2dc1c3dc9579cf8cc497013b89ff215b42ea28a3
Shilpa Tripathi <shilpat@uci.edu>
2018-05-16 18:00:48 -0700

    deleted lock

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var static = require('node-static');
 var http = require('http');
 /* Assume that we are running on Heroku */
 var port = process.env.PORT;
-var directory = __dirname + './public';
+var directory = __dirname + '/public';
 /* If we are not on Heroku, then we need to readjust the port and directory
 *information and we know that because port won't be set */
 if (typeof port == 'undefined' || !port) {
