90448adcec21a6cc262f411e44730613854b2f90
Shilpa Tripathi <shilpat@uci.edu>
2018-06-11 15:52:19 -0700

    flipping tokens, elapsed time, game rules

--- public/js/main.js
+++ public/js/main.js
@@ -331,6 +331,7 @@ var old_board = [
 ];
 
 var my_color = ' ';
+var interval_timer; //set on final day
 
 socket.on('game_update', function(payload){
 	console.log('*** Client Log Message: \'game_update\' \n\tpayload: '+JSON.stringify(payload));
@@ -362,6 +363,27 @@ socket.on('game_update', function(payload){
 	}
 	
 	$('#my_color').html('<h3 id="my_color">I am '+my_color+'</h3>');
+	$('#my_color').append('<h4>It is '+payload.game.whose_turn+'\'s turn. Elapsed time <span id="elapsed"></span></h4>'); //set on final day
+
+	//Elapsed Time
+	
+	clearInterval(interval_timer);
+	interval_timer=setInterval(function(last_time){
+		return function(){
+			//Do the work of updating the UI
+			var d=new Date();
+			var elapsedmilli=d.getTime()-last_time;
+			var minutes=Math.floor(elapsedmilli/(60*1000));
+			var seconds=Math.floor((elapsedmilli%(60*1000))/1000);
+			
+			if(seconds<10){
+				$('#elapsed').html(minutes+':0'+seconds);
+			}
+			else{
+				$('#elapsed').html(minutes+':'+seconds);
+			}
+		}} (payload.game.last_move_time)
+	,1000);
 	
 	// Animate changes to the board
 	
@@ -403,10 +425,13 @@ socket.on('game_update', function(payload){
 				else {
 					$('#'+row+'_'+column).html('<img src="assets/images/error.gif" alt="white square"> alt="error"/>');
 				}
-				
-				// Set up interactivity
-				$('#'+row+'_'+column).off('click');
-				if (board[row][column] === ' '){
+		}
+	// Set up interactivity
+	$('#'+row+'_'+column).off('click');
+	$('#'+row+'_'+column).removeClass('hovered_over');
+	
+	if(payload.game.whose_turn===my_color){
+		if(payload.game.legal_moves[row][column]===my_color.substr(0,1)){
 					$('#'+row+'_'+column).addClass('hovered_over');
 					$('#'+row+'_'+column).click(function(r,c){
 						return function(){
@@ -414,17 +439,16 @@ socket.on('game_update', function(payload){
 							payload.row = r;
 							payload.column = c;
 							payload.color = my_color;
-							console.log('*** Client Log Message: \'Play_token\' payload: '+JSON.stringify(payload));
+							console.log('*** Client Log Message: \'Play_token\' payload:'+JSON.stringify(payload));
 							socket.emit('play_token', payload);
 						};
 					}(row,column));
+						}
 				}
-				else {
-					$('#'+row+'_'+column).removeClass('hovered_over');
-				}
-			}
+			}				
 		}
 	}
+			
 	$('#blacksum').html(blacksum);
 	$('#whitesum').html(whitesum);
 	
